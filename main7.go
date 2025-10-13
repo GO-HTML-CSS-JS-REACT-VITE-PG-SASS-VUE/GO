015420a9aa8c407a24bdd53a8adb28731037cbe7
ришат <abzalimovrrrr@yandex.ru>
2025-10-13 11:06:15 +0300

    main7.go

---
+++ main7.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+    age := 18
+    if age >= 18 {                    // Простое условие
+        fmt.Println("Совершеннолетний")
+    } else {
+        fmt.Println("Несовершеннолетний")
+    }
+}
