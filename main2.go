67f27bd44d2a285f53a48e64da949e85224380b6
ришат <abzalimovrrrr@yandex.ru>
2025-10-13 10:19:47 +0300

    main3.go

--- main2.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-func main() {
-    var name string = "Alice"  // Явное объявление переменной
-    age := 25                  // Короткое объявление с выводом типа
-    fmt.Println(name, age)     // Вывод значений
-}
